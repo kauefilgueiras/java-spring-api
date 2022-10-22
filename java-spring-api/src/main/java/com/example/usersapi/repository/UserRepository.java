c4fca0f6fb56aee14413061a38b87411f93c286c
kauefilgueiras <kauefilgueiras@gmail.com>
2022-10-22 02:25:47 -0300

    new update

--- java-spring-api/src/main/java/com/example/usersapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.usersapi.repository;
-
-import com.example.usersapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
